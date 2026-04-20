90fcb6a321219776d513da1e03242828f9331eec
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-04-21 00:07:00 +0530

    SHELLSCRIPT-ROBOSHOP

---
+++ Roboshop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-0e24fb4b2e1f13c94"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z04803303VFTPZATMTQ6X"
+DOMAIN_NAME="devopsaws.store"
+
+for instance in ${INSTANCES[@]}
+do  
+  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t2.micro --security-group-ids sg-0e24fb4b2e1f13c94 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
+
+  if [ $instance != "frontend" ]
+  then
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+  else
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+  fi 
+  echo "$instance ip address is $IP"
+
+done
