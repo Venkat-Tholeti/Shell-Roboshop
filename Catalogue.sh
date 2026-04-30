dc084d63e03bc9f76af04cb78c779e1841053081
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-04-30 22:41:18 +0530

    Shell-Roboshop

--- Catalogue.sh
+++ Catalogue.sh
@@ -81,6 +81,15 @@ VALIDATE $? "Copying MongoDb Repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing Mongodb Client"
 
+STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.devopsaws.store </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading data into MongoDB"
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N"
+fi
+
 mongosh --host mongodb.devopsaws.store </app/db/master-data.js
 VALIDATE $? "Load Master Data of the List of products"
 
