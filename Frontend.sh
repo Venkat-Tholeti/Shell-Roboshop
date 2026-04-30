b4db0c315004e4d18d561b5d1d1191e9f65ac315
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-04-30 22:54:53 +0530

    Shell-Roboshop

---
+++ Frontend.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIRECTORY=$PWD
+
+
+mkdir -p $LOGS_FOLDER
+
+echo -e "$G Script Started Executing At $(date) $N" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then  
+  echo -e  "$R ERROR: Please run this script with root access $N" | tee -a $LOG_FILE
+  exit 1 # we can give any number upto 127 other than 0 
+else
+  echo -e  "$G You are Running with root access $N" | tee -a $LOG_FILE
+fi
+
+#FUNCTION FOR REPEATED CODE
+VALIDATE(){
+    if [ $1 -eq 0 ] # We can pass arguments to function also , Here $1 is 1st argument ($?) of VALIDATE in Installed space below
+  then
+    echo -e   "$G  $2 is success $N" | tee -a $LOG_FILE # $2 is Software mentioned as below in VALIDATE function in INSTALLED SPACE BELOW
+  else
+    echo -e  "$R  $2 is failure $N" | tee -a $LOG_FILE
+    exit 1
+  fi
+
+}
+
+
+dnf module disable nginx -y &>>$LOG_FILE
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Nginx installation"
+
+systemctl enable nginx 
+systemctl start nginx 
+VALIDATE $? "Enable & Starting nginx service"
+
+rm -rf /usr/share/nginx/html/* 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Download and Extract the frontend content"
+
+rm -rf /etc/nginx/nginx.conf
+cp $SCRIPT_DIRECTORY/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copying Nginx Conf File"
+
+systemctl restart nginx 
+VALIDATE $? "Nginx Service restart"
