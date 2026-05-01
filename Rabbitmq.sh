e23f93ba6443609dc7712c4b5973a87983ab9e6d
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-05-01 15:22:57 +0530

    Shell-Roboshop

---
+++ Rabbitmq.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+
+mkdir -p $LOGS_FOLDER
+
+echo -e "$G Script Started Executing At $(date) $N" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then  
+  echo -e  "$R ERROR: Please run this script with root access $N" | tee -a $LOG_FILE
+  exit 1 # we can give any number upto 127 other than 0 
+else
+  echo -e  "$G You are Running with root access $N" | tee -a $LOG_FILE
+fi
+
+#FUNCTION FOR REPEATED CODE
+VALIDATE(){
+    if [ $1 -eq 0 ] # We can pass arguments to function also , Here $1 is 1st argument ($?) of VALIDATE in Installed space below
+  then
+    echo -e   "$G  $2 is success $N" | tee -a $LOG_FILE # $2 is Software mentioned as below in VALIDATE function in INSTALLED SPACE BELOW
+  else
+    echo -e  "$R  $2 is failure $N" | tee -a $LOG_FILE
+    exit 1
+  fi
+
+}
+
+echo "Please Enter Password for RabbitMq"
+read -s RabbitMq_PASSWORD 
+
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Rabbitmq Repo Copy"
+
+dnf install rabbitmq-server -y
+VALIDATE $? "Installation Of Rabbitmq"
+
+systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
+VALIDATE $? "Enable & Starting of Rabbitmq service"
+
+rabbitmqctl add_user roboshop $RabbitMq_PASSWORD
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
